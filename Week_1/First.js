ac8aec73dd1ceab4eb58641c32e2de9ae3c40005
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2024-10-08 13:15:40 +0530

    basics of objects

--- Week_1/First.js
+++ Week_1/First.js
@@ -1,2 +1,17 @@
-console.log("HELOO");
+const user=[{
+    firstname:"ayusman",
+    gender:"male"
+},{
+    firstname:"mitesh",
+    gender:"male"
+},{
+    firstname:"rando",
+    gender:"female"
+}]
+
+for(let i=0;i<user.length;i++){
+    if(user[i]["gender"]=="male"){
+        console.log(user[i]["firstname"]);
+    }
+}
 
