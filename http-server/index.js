8d0a04611c965ae3d9674bbafa0dbd366ee96227
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2024-11-04 19:58:24 +0530

    http server

---
+++ http-server/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const port=3000
+const app = express()
+
+app.use(express.json());
+
+app.get('/',function(req,res){
+    res.send("heloo world")
+})
+
+app.listen(port)
+
