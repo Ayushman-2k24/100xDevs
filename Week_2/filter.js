99be4488ba38f4a76ce3af9b18fee9db95b0b580
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2024-11-08 23:04:32 +0530

    variation of filter

--- Week_2/filter.js
+++ Week_2/filter.js
@@ -4,6 +4,16 @@ const arr=[1,2,3,4,5]
 
 const ans=arr.filter(num=>num>2)
 console.log(ans);
+console.log("\n");
+
+
+const ans1=arr.filter((num)=>{
+    if (num>0) {
+        return true
+    }
+    return false
+})
+console.log(ans);
 
 
 
