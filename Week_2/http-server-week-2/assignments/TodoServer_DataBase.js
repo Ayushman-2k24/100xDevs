5fe892f6a9ae3e220c2e9cb4ca075029dda8b8e3
Ayushman-2k24 <ayushmansenapati7@gmail.com>
2025-03-12 13:44:17 +0530

    90 percent of todo with external databse completed

--- Week_2/http-server-week-2/assignments/TodoServer_DataBase.js
+++ Week_2/http-server-week-2/assignments/TodoServer_DataBase.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const fs=require("fs")
 const path=require("path")
+const { json } = require("stream/consumers")
 // const { stringify } = require("querystring")
 const app=express()
 app.use(express.json())
@@ -9,13 +10,10 @@ var filepath=path.join(__dirname,"files","data.json")
 app.get("/todos",function(req,res){
     fs.readFile(filepath,"utf-8",function(err,data){
         if(err){
-            res.status(404).json({
-                msg:"no data found"
-            })
+            res.status(500).json({msg:"error reading file"})
         }
-        res.status(200).json(
-            JSON.parse(data)
-        )
+        const todos=JSON.parse(data)
+        res.status(200).json(todos)
     })
 })
 
@@ -23,18 +21,20 @@ app.get('/todos/:id',function(req,res){
     
     fs.readFile(filepath,"utf-8",function(err,data){
         if(err){
-            res.json({
-                msg:"data not found"
+            res.status(500).json({
+                msg:"error reading file"
             })
         }
     
         var id=parseInt(req.params.id)
         var todos=JSON.parse(data)
         var todo=todos.find(item=>item.id==id)
+
         if(!todo){
             res.status(404).json({msg:"todo not found"})
         }
-        res.json(todo)
+
+        res.status(200).json(todo)
     })
 })
 
@@ -42,25 +42,30 @@ app.post('/todos',function(req,res){
 
     fs.readFile(filepath,"utf-8",function(err,data){
 
-        if(err){res.json({msg:"error reading file"})}
+        if(err){res.status(500).json({msg:"error reading file"})}
         
         let todos=JSON.parse(data)
+
+        if(!req.body.title || typeof req.body.completion !=="boolean"){
+            return res.status(400).json({msg:"title and completion are required"})
+        }
+
         let newId=todos.length>0? todos[todos.length-1].id+1:1
 
         let newtodo={
             id:newId,
             title:req.body.title,
             completion:req.body.completion,
-            description:req.body.description
+            description:req.body.description || ""
         }
 
         todos.push(newtodo)
 
         fs.writeFile(filepath,JSON.stringify(todos),function(err){
             if(err){
-                res.json({msg:"error writing file"})
+                return res.status(500).json({msg:"error writing file"})
             }else{
-                res.json(newtodo)
+                res.status(201).json({msg:"todo created successfully",newtodo})
             }
         })
     })
@@ -68,16 +73,23 @@ app.post('/todos',function(req,res){
 
 app.put('/todos/:id',function(req,res){
     fs.readFile(filepath,"utf-8",function(err,data){
-        if(err){return res.json({msg:"error reading file"})}
-        let todos=JSON.parse(data)
+
+        
+    if(err){return res.status(500).json({msg:"error reading file"})}
+    
+    let todos=JSON.parse(data)
         let id=parseInt(req.params.id)
         let index=todos.findIndex(item=>item.id==id)
         
         
         if(index==-1){
-            return res.json({msg:"todo not found"})
+            return res.status(404).json({msg:"todo not found"})
         }
  
+        if(!req.body.title && req.body.completion == undefined && !req.body.description){
+            return res.status(400).json({msg:"provide atlest one field for update"})
+        }
+
         todos[index]={
             ...todos[index],
             title:req.body.title??todos[index].title,
@@ -87,13 +99,36 @@ app.put('/todos/:id',function(req,res){
         
         fs.writeFile(filepath,JSON.stringify(todos),function(err){
             if(err){
-                return res.json({msg:"error updating the file"})
+                return res.status(500).json({msg:"error updating the file"})
             }
             else{
-                res.json({msg:"updated the todos successfully"})
+                res.status(200).json({msg:"updated the todos successfully"})
             }
         })
 
+    })
+})
+
+app.delete("/todos/:id",function(req,res){
+    fs.readFile(filepath,"utf-8",function(err,data){
+        if(err){
+            return res.status(500).json({msg:"error reading file"})
+        }
+
+        let todos=JSON.parse(data)
+        const id=parseInt(req.params.id)
+        const updatedtodos=todos.filter(item=>item.id!==id)
+
+        if(todos.length==updatedtodos.length){
+            return res.status(404).json({msg:"todo not found"})
+        }
+
+        fs.writeFile(filepath,JSON.stringify(updatedtodos),function(err){
+            if(err){
+                return res.status(500).json({msg:"error reading file"})
+            }
+            res.status(200).json({msg:"todo deleted successfully"})
+        })
     })
 })
 
